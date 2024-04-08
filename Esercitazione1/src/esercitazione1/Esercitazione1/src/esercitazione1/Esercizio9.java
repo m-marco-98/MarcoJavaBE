95b5eb1c4ad9aae254e96f9a52f71ec0a8c6e7df
m-marco-98 <165819009+m-marco-98@users.noreply.github.com>
2024-04-08 23:20:06 +0200

    Consegna da casa

---
+++ Esercitazione1/src/esercitazione1/Esercitazione1/src/esercitazione1/Esercizio9.java
@@ -0,0 +1,28 @@
+package esercitazione1;
+
+import java.util.Scanner;
+
+public class Esercizio9 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Quante righe vuoi?");
+		int righe = input.nextInt();
+		
+		for (int i = 0; i <= righe; i++) {
+			
+			for (int j = 1; j <= i; j++) {
+				
+				System.out.print(j);
+				
+			}
+			System.out.println("");			
+		}
+		
+		
+		
+	}
+
+}
