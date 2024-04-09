bcaefeb3ab311f09454958eb50f71f4f52dda91c
m-marco-98 <165819009+m-marco-98@users.noreply.github.com>
2024-04-09 11:48:53 +0200

    Update Esercizio9.java

--- Esercitazione1/src/esercitazione1/Esercizio9.java
+++ Esercitazione1/src/esercitazione1/Esercizio9.java
@@ -10,15 +10,18 @@ public class Esercizio9 {
 		
 		System.out.println("Quante righe vuoi?");
 		int righe = input.nextInt();
-		
+
+		//Auemnta i di uno per ogni ciclo, fino a quando non arriva al numero di righe desiderato, alla fine di ogni riga va a capo
 		for (int i = 0; i <= righe; i++) {
-			
+
+			//Stampa i numeri da j (a inizio ciclo è sempre 1) fino a i (che aumenta di uno alla fine di ogni ciclo padre)
+			//sulla stessa riga.
 			for (int j = 1; j <= i; j++) {
 				
 				System.out.print(j);
 				
 			}
-			System.out.println("");			
+			System.out.println("");	//va a capo per la riga successiva		
 		}
 		
 		
