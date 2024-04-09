5303a8a41e621a08af52c6cf691097faeb90f832
m-marco-98 <165819009+m-marco-98@users.noreply.github.com>
2024-04-09 11:37:21 +0200

    Update Esercizio3.java

--- Esercitazione1/src/esercitazione1/Esercizio3.java
+++ Esercitazione1/src/esercitazione1/Esercizio3.java
@@ -11,7 +11,9 @@ public class Esercizio3 {
 		int numero = input.nextInt();
 		
 		int contatore = 1;
-		for (int i = 1; i < numero/2; i++) {
+
+		//Calcolo il resto del numero / i, se è uguale a 0 allora è un divisore e lo conta. Arriva fino alla metà del numero, perchè perchè essa è il suo divisore massimo
+		for (int i = 1; i <= numero/2; i++) {
 			if (numero%i == 0) {
 				System.out.print(i+"  ");
 				contatore++;
