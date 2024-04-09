5b8fefa23f854ee7e50a9ce611ede0ee21fa079c
m-marco-98 <165819009+m-marco-98@users.noreply.github.com>
2024-04-09 11:55:27 +0200

    Update Esercizio10.java

--- Esercitazione1/src/esercitazione1/Esercizio10.java
+++ Esercitazione1/src/esercitazione1/Esercizio10.java
@@ -14,8 +14,11 @@ public class Esercizio10 {
 		int i = 0;
 		int j = 0;
 		int k = 0;
-		
 
+		//Esegue il ciclo fino a quando i è minore del numero inserito, 
+		//a ogni cilco aumenta k, ciò fa in modo che ogni riga abbia un elemento in più della precedente
+		//in quanto il while figlio stampa numeri (i) sulla stessa riga fino a quando j è minore di k
+		
 		while (i <= numeri) {
 			
 			k++;
@@ -28,6 +31,9 @@ public class Esercizio10 {
 			j = 0;
 			System.out.println();
 		} 
+		//N.B. Stampa numeri uguali a i e non uguali a j poichè j indica solo quanti numeri stampare su quella riga
+		//ma la serie deve continuare a scorrere a ogni rga, e non riiniziare, j invece si azzera all afine del ciclo
+		//in modo da stampare tanti numeri uanto è k, che aumenta ogni ciclo superiore
 		
 	}
 
