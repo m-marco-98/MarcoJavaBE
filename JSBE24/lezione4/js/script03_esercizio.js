51327947980ee1e120664e34fe77dfa8fa3930c7
m-marco-98 <165819009+m-marco-98@users.noreply.github.com>
2024-06-07 17:50:06 +0200

    Upload da aula

---
+++ JSBE24/lezione4/js/script03_esercizio.js
@@ -0,0 +1,27 @@
+const URL = "https://reqres.in/api/users?page=2";
+
+//Utilizzo fetch API per poter chiamare i dati e ottenerli
+//Applico il metodo GET
+
+let utenti = [];
+
+let elListaUtenti = document.querySelector("#listaUtenti");
+
+fetch(URL)
+    .then(data => {
+        console.log(data)
+        return data.json(); //Il metodo json converte il dato formato json in obj
+    }
+    )
+    .then(response => {
+        console.log(response);
+        //Stampo i nomi degli utenti
+        response.data.forEach(user => {
+            
+            //Controllo
+            console.log(user.first_name + " " + user.last_name);  
+            //Stampa in HTML
+            elListaUtenti.innerHTML += stampaInfoUser(user);
+        });
+
+    });
